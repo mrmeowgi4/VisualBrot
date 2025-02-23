8a12ac9c5e051fb9f71b9623c1fb939b16a62251
ssain <ssain@MSI>
2025-02-22 19:29:42 -0800

    Add project files.

---
+++ VisualBrot/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace VisualBrot
+{
+    partial class VisualBrot
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisualBrot));
+            panel1 = new Panel();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel1.Cursor = Cursors.IBeam;
+            panel1.Location = new Point(394, 88);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(500, 393);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 146);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(376, 45);
+            trackBar1.TabIndex = 2;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 128);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Number of iterations";
+            label1.Click += label1_Click;
+            // 
+            // VisualBrot
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1078, 681);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.ControlText;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "VisualBrot";
+            Text = "VisualBrot";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TrackBar trackBar1;
+        private Label label1;
+    }
+}
---
+++ VisualBrot/Form1.cs
@@ -0,0 +1,124 @@
+using System.Globalization;
+using System.Numerics;
+using System.Xml.Serialization;
+namespace VisualBrot
+{
+    public partial class VisualBrot : Form
+    {
+        private readonly int Width;
+        private readonly int Height;
+        private readonly Pen Pencil = new Pen(Color.Red);
+        private Graphics View;
+        private Bitmap Bmp;
+
+        private readonly Color[] Colors = { Color.DarkBlue, Color.Black, Color.Gold, Color.BlueViolet };
+
+        public VisualBrot()
+        {
+            InitializeComponent();
+            View = panel1.CreateGraphics();
+            Width = panel1.Width;
+            Height = panel1.Height;
+
+            Bmp = new Bitmap(Width, Height);
+
+
+
+
+            panel1.Paint += panel1_Paint;
+
+        }
+
+        private void MBrot(int max, double xmin, double xmax, double ymin, double ymax)
+        {
+            double realComponent;
+            double imaginaryComponent;
+
+            int iterationCount;
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+
+                    // Convert the imaginary plane mappings to form coords.
+                    realComponent = xmin + (x / (double)Width) * (xmax - xmin);
+                    imaginaryComponent = ymax - (y / (double)Height) * (ymax - ymin);
+
+                    var c = new Complex(realComponent, imaginaryComponent);
+                    var z = c;
+
+                    iterationCount = 0;
+
+                    while (Complex.Abs(z) <= 2 && iterationCount < max)
+                    {
+                        z = (z * z) + c;
+
+                        realComponent = z.Real;
+                        imaginaryComponent = z.Imaginary;
+
+                        iterationCount++;
+                    }
+
+                    int choice = 0;
+                    if (iterationCount == max)
+                    {
+                        choice = 3;
+                    }
+                    else
+                    {
+                        choice = (iterationCount * Colors.Length) / max;
+                    }
+
+                    Bmp.SetPixel(x, y, Colors[choice]);
+
+                }
+
+                panel1.Invalidate(); // redraw
+            }
+
+            //Save image to disk.
+            Bmp.Save("VisualBrot result.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Bmp, 0, 0);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            panel1.Scale(new SizeF(1.1f, 1.1f));
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int level = trackBar1.Value;
+            
+            MBrot(level * 10, -1.5, 1, -1.5, 1);
+            panel1.CreateGraphics().DrawImage(Bmp, 0, 0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
